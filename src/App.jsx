75067ec0a7f2c95a9234f422e63a2500d43ddccc
Randy.Vargas96 <randyvargas@pursuit.org>
2023-08-03 13:51:07 -0400

    first commit, code from class example, cat photo as placeholder

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
+import data from "./data/data.json"
+
 
 function App() {
+//js
+  // WILL use state
+
+  const studentTorender = data.map(
+    
+    (eachStudentObj) => {
+      return(<h4>MONEYYYYYYYYYY</h4>)
+    }
+  )
   return (
+
     <div>
+      <img src="https://cdn.freecodecamp.org/curriculum/cat-photo-app/cats.jpg" alt="Five cats looking around a field."/>
       <h1>Student Dashboard</h1>
+
     </div>
+
   );
 }
 
